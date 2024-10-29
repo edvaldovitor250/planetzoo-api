ce17768b9afbdfb962d86aaad9878aff28e28d38
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-29 16:39:22 -0300

    feat: Refactor Animal class to remove duplicate constructor and use Lombok annotations

--- src/main/java/com/edvaldovitor/PlanetZoo/model/Animal.java
+++ src/main/java/com/edvaldovitor/PlanetZoo/model/Animal.java
@@ -1,80 +1,16 @@
 package com.edvaldovitor.PlanetZoo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
-@NoArgsConstructor
-@AllArgsConstructor
 @Component
+@AllArgsConstructor
+@NoArgsConstructor
 @Scope("prototype")
+@Data
 public class Animal {
-
     private int id;
     private String name;
     private String species;
-
-    public Animal() {
-    }
-
-    public Animal(int id, String name, String species) {
-        if (id <= 0) {
-            throw new IllegalArgumentException("ID must be a positive integer.");
-        }
-        this.id = id;
-        this.name = name != null ? name : "Unknown";
-        this.species = species != null ? species : "Unknown";
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        if (id <= 0) {
-            throw new IllegalArgumentException("ID must be a positive integer.");
-        }
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name != null ? name : "Unknown";
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species != null ? species : "Unknown";
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", species='" + species + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Animal animal = (Animal) o;
-        return id == animal.id && Objects.equals(name, animal.name) && Objects.equals(species, animal.species);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, species);
-    }
 }
