895a2109ea7146d364ec0d7a251b979411edb90e
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-29 13:24:35 -0300

    feat: create animal model and configuration

---
+++ src/main/java/com/edvaldovitor/PlanetZoo/model/Animal.java
@@ -0,0 +1,41 @@
+package com.edvaldovitor.PlanetZoo.model;
+
+public class Animal {
+
+    private int id;
+    private String name;
+    private String species;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", species='" + species + '\'' +
+                '}';
+    }
+}
