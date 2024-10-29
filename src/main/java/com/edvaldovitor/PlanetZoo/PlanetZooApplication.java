690a3cdc58069a4b3c64a23a6df37fecf8506ee0
Edvaldo Vitor De Castro Souza <116117189+edvaldovitor250@users.noreply.github.com>
2024-10-29 13:13:58 -0300

    feat: start project

---
+++ src/main/java/com/edvaldovitor/PlanetZoo/PlanetZooApplication.java
@@ -0,0 +1,13 @@
+package com.edvaldovitor.PlanetZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edvaldovitor/PlanetZoo/PlanetZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldovitor.PlanetZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
