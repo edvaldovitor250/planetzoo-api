7f0140439fae4c44ba9fe1dc204073c6989a1015
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-29 13:56:45 -0300

    feat: added JDBC insert Animal Repository

--- src/main/java/com/edvaldovitor/PlanetZoo/AnimalRepository.java
+++ src/main/java/com/edvaldovitor/PlanetZoo/AnimalRepository.java
@@ -1,20 +1,33 @@
 package com.edvaldovitor.PlanetZoo;
 
 import com.edvaldovitor.PlanetZoo.model.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Repository
 public class AnimalRepository {
 
+    private JdbcTemplate template;
 
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    @Autowired
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
 
     public void save(Animal animal){
-        System.out.println("add");
+
+        String sql = "INSERT INTO animal (id, name, species) VALUES (? ,? ,?)";
+
+
+        template.update(sql,animal.getId(),animal.getName(),animal.getSpecies());
     }
 
     public List<Animal> findAll(){
